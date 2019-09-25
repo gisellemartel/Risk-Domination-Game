616b23bf4aa79da054a9cc0afe0437d39b0a878f
Wayne <waynevick7226@hotmail.com>
2019-09-24 23:37:04 -0400

    added functions on map

--- Part1/Map.cpp
+++ Part1/Map.cpp
@@ -3,5 +3,46 @@
 //
 
 #include "Map.h"
+#include "Player.h"
+
+using namespace std;
+
+class map{
+
+};
+
+class Country{
+private:
+    string country_name;
+    Player country_owner;
+    int numberOfArmies;
+    vector<Country> neighbors
+
+public:
+    Country(string country_name){
+        this->country_name = country_name;
+        this->country_owner = null;
+        this->numberOfArmies = 0;
+    }
+
+    void setCountryName(string country_name){
+        this->country_name = country_name;
+    }
+    string getCountryName(){
+        return this->country_name;
+    }
+
+    void setNumberOfArmies(int numberOfArmies){
+        this->numberOfArmies = numberOfArmies;
+    }
+
+    int getNumberOfArmies(){
+        return this->numberOfArmies;
+    }
+
+    void addNeighborCountry(Country neighbor){
+
+    }
 
 
+};
