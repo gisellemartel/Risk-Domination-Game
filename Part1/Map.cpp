4c21cb5c5b87fad7c45634681250b2516b579ce0
Wayne <waynevick7226@hotmail.com>
2019-09-25 21:12:51 -0400

    added lines

--- Part1/Map.cpp
+++ Part1/Map.cpp
@@ -4,19 +4,53 @@
 
 #include "Map.h"
 #include "Player.h"
+#include <list>
 
 using namespace std;
 
 class map{
+private:
+    list <Continent> continents;
+
+public:
+    void addContinent(Continent continent){
+        continents.push_front(continent);
+    }
+
 
 };
 
+class Continent{
+private:
+    string continent_name;
+    list <Country> countriesInContinent;
+
+public:
+    Continent(string continent){
+        this->continent_name = continent;
+    }
+
+    void setContinentName(string continentName){
+        this->continent_name = continentName;
+    }
+
+    string getContinentName(){
+        return this->continent_name;
+    }
+
+    void addCountryToContinent(Country country){
+        //need to add condition to check if country is already in any other continent
+        countriesInContinent.push_back(country);
+    }
+
+};
 class Country{
 private:
     string country_name;
     Player country_owner;
     int numberOfArmies;
-    vector<Country> neighbors
+    list <Country> neighbors;
+    Continent continent;
 
 public:
     Country(string country_name){
@@ -41,8 +75,20 @@ public:
     }
 
     void addNeighborCountry(Country neighbor){
-
+        neighbors.push_front(neighbor);
     }
 
+    bool isNeighbor(Country neighbor){
+
+        return false;
+    }
+
+    void belongsToContinent(Continent continent){
+        this->continent = continent;
+    }
+
+    Continent getContinent(){
+        return this=>continent ;
+    }
 
 };
--- Part1/MapDriver.cpp
+++ Part1/MapDriver.cpp
@@ -4,9 +4,20 @@
 
 #include "Map.h"
 #include <iostream>
+#include <list>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+    list <int> numbers;
+    numbers.push_back(4);
+    numbers.push_back(10);
+    numbers.push_back(22);
+
+    list<int>::iterator searchedItem = find(numbers.begin(), numbers.end(), 10);
+
+    cout <<  << endl;
+
     cout << "Map Driver" << endl;
 }
