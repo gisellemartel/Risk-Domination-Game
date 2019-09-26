65e87de6fd46dd86db26cd08d66022b562c7d9b9
Giselle Martel <gmartel90@gmail.com>
2019-09-25 22:50:29 -0400

    refactored class definitions in .h file, changed from copies to pointer
    '

--- Part1/Map.cpp
+++ Part1/Map.cpp
@@ -4,91 +4,31 @@
 
 #include "Map.h"
 #include "Player.h"
-#include <list>
 
 using namespace std;
 
-class map{
-private:
-    list <Continent> continents;
-
-public:
-    void addContinent(Continent continent){
-        continents.push_front(continent);
-    }
+//Map class method implementations ----------------------
+void Map::addContinent(Continent* continent) {
+    continents.push_front(continent);
+}
 
 
-};
+//Country class method implementations ----------------------
+void Country::addNeighborCountry(const Country* neighbor) {
+    neighbors.push_front(neighbor);
+}
 
-class Continent{
-private:
-    string continent_name;
-    list <Country> countriesInContinent;
+bool Country::isNeighbor(const Country* neighbor) {
+    return false;
+}
 
-public:
-    Continent(string continent){
-        this->continent_name = continent;
-    }
+bool Country::belongsToContinent(const Continent* continent) {
+    return false;
+}
 
-    void setContinentName(string continentName){
-        this->continent_name = continentName;
-    }
+//Continent class method implementations ----------------------
+void Continent::addCountryToContinent(Country* country){
+    //need to add condition to check if country is already in any other continent
+    countriesInContinent.push_back(country);
+}
 
-    string getContinentName(){
-        return this->continent_name;
-    }
-
-    void addCountryToContinent(Country country){
-        //need to add condition to check if country is already in any other continent
-        countriesInContinent.push_back(country);
-    }
-
-};
-class Country{
-private:
-    string country_name;
-    Player country_owner;
-    int numberOfArmies;
-    list <Country> neighbors;
-    Continent continent;
-
-public:
-    Country(string country_name){
-        this->country_name = country_name;
-        this->country_owner = null;
-        this->numberOfArmies = 0;
-    }
-
-    void setCountryName(string country_name){
-        this->country_name = country_name;
-    }
-    string getCountryName(){
-        return this->country_name;
-    }
-
-    void setNumberOfArmies(int numberOfArmies){
-        this->numberOfArmies = numberOfArmies;
-    }
-
-    int getNumberOfArmies(){
-        return this->numberOfArmies;
-    }
-
-    void addNeighborCountry(Country neighbor){
-        neighbors.push_front(neighbor);
-    }
-
-    bool isNeighbor(Country neighbor){
-
-        return false;
-    }
-
-    void belongsToContinent(Continent continent){
-        this->continent = continent;
-    }
-
-    Continent getContinent(){
-        return this=>continent ;
-    }
-
-};
--- Part1/Map.h
+++ Part1/Map.h
@@ -5,12 +5,92 @@
 #ifndef TEAMPROJECT_COMP345_MAP_H
 #define TEAMPROJECT_COMP345_MAP_H
 
-#endif //TEAMPROJECT_COMP345_MAP_H
 
-class Map
-{
-public:
+#include "Part4/Player.h"
+#include <list>
+#include <string>
+
+using namespace std;
+
+//Country class ------------------------------------------------------------------------------------
+class Country{
 
 private:
+    string* country_name;
+    Player* country_owner;
+    int number_of_armies;
+    list<Country*> neighbors;
+    Continent* continent;
+
+public:
+    //Constructor using initializer list
+    Country(string* in_country_name) :
+        country_name(in_country_name),
+        country_owner(nullptr),
+        number_of_armies(0)
+        {}
+
+    //Setters --------------------------------------------------
+
+    void setCountryName(string* in_country_name){ country_name = in_country_name; }
+
+    void setNumberOfArmies(int in_number_of_armies){ number_of_armies = in_number_of_armies; }
+
+    //Getters --------------------------------------------------
+
+    const string* getCountryName() const { return country_name; }
+
+    const int getNumberOfArmies() const { return number_of_armies; }
+
+    const Continent* getContinent() const { return continent; }
+
+    //Methods -------------------------------------------------------
+
+    void addNeighborCountry(const Country* neighbor);
+
+    bool isNeighbor(const Country* neighbor);
+
+    bool belongsToContinent(const Continent* continent);
+};
+
+
+//Continent class ------------------------------------------------------------------------------------
+class Continent{
+private:
+    string* continent_name;
+    list<Country*> countries_in_continent;
+
+public:
+    //Constructor using initializer list
+    Continent(string* in_continent_name)
+        : continent_name (in_continent_name)
+        {}
+
+    //Setters --------------------------------------------------
+    void setContinentName(string* in_continent_name) { continent_name = in_continent_name; }
+
+
+    //Getters --------------------------------------------------
+    const string* getContinentName() const { return continent_name; }
+
+
+    //Methods -------------------------------------------------------
+    void addCountryToContinent(Country* country);
+};
+
+
+//Map class ------------------------------------------------------------------------------------
+class Map
+{
+private:
+    list<Continent*> continents;
+
+public:
+
+    //Methods -------------------------------------------------------
+    void addContinent(Continent* continent);
 
 };
+#endif //TEAMPROJECT_COMP345_MAP_H
+
+
