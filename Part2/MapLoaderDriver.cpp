62ff3a86fafbc059b64a3960451703152f37ff38
Giselle Martel <gmartel90@gmail.com>
2019-09-26 02:42:54 -0400

    code cleanup, created graph data structure for map

--- Part2/MapLoaderDriver.cpp
+++ Part2/MapLoaderDriver.cpp
@@ -1,6 +1,8 @@
-//
-// Created by Giselle Martel on 2019-09-13.
-//
+/**
+ * Assignment #1 COMP345, FALL 2019
+ * Authors: Giselle Martel (26352936), Wayne Tam, Jeffrey Li, Rania Az
+ */
+
 #include <iostream>
 using namespace std;
 
--- cmake-build-debug/CMakeFiles/Part4.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Part4.
-# This may be replaced when dependencies are built.
