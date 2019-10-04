a666802000b419f32e3d057b2e65a7001e8ffeaf
wayne1tam <waynevick7226@hotmail.com>
2019-10-04 18:50:37 -0400

    adding continents and countries with valid parameters is functional

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -16,11 +16,15 @@ int main()
 
     Map* america = new Map("America map", 9, 5);
     //name, army value, id
-    america->AddContinentToMap("North America", 4, 1);
-    america->AddContinentToMap("South America", 3, 2);
+    america->AddContinentToMap("North America", 5, 1);
+    america->AddContinentToMap("South America", 2, 2);
+    america->AddContinentToMap("Europe", 5, 3);
     america->DisplayContinents();
 
-    america->AddCountryToMap();
+    america->AddCountryToMap(1,"Alaska", 1);
+    america->AddCountryToMap(2, "Alberta", 1);
+    america->AddCountryToMap(3, "Central America", 1);
+    america->DisplayCountries();
 
     delete america;
     cout << "Map Driver" << endl;
