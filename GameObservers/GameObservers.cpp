b65e4fda3586e86ea4d9c66806b372cc59abcc5a
wayne1tam <waynevick7226@hotmail.com>
2019-11-14 13:33:53 -0500

    GameStatisticObserver

--- GameObservers/GameObservers.cpp
+++ GameObservers/GameObservers.cpp
@@ -3,3 +3,48 @@
  * Project: Risk Domination Game
  * Authors: Giselle Martel (26352936), Wayne Tam (21308688), Jeffrey Li (40017627), Rania Az (40041630)
  */
+#include <iostream>
+#include "GameObservers.h"
+#include "../Player/Player.h"
+
+using namespace std;
+
+Observer::Observer() {
+};
+Observer::~Observer() {
+};
+
+Subject::Subject(){
+    observers_ = new vector<Observer*>;
+}
+
+Subject::~Subject(){
+    delete observers_;
+}
+
+void Subject::Attach(Observer* o){
+    observers_->push_back(o);
+}
+
+void Subject::Detach(Observer* o){
+
+}
+
+void Subject::Notify(){
+    vector<Observer*>::iterator i = observers_->begin();
+    for (; i!= observers_->end(); ++i)
+        (*i)->Update();
+}
+
+GameStatisticObserver::GameStatisticObserver(Player players){
+    Players_ = new vector<Player*>;
+}
+
+void GameStatisticObserver::update(){
+    for(int i= 0; i< Players_->size() ; i++){
+        cout<<Players_->at(i)->GetPlayerName();
+
+        cout<<Players_->at(i)->GetPlayerName();
+    }
+}
+
--- GameObservers/GameObservers.h
+++ GameObservers/GameObservers.h
@@ -3,8 +3,57 @@
  * Project: Risk Domination Game
  * Authors: Giselle Martel (26352936), Wayne Tam (21308688), Jeffrey Li (40017627), Rania Az (40041630)
  */
+#pragma once
 
-#ifndef TEAMPROJECT_COMP345_GAMEOBSERVERS_H
-#define TEAMPROJECT_COMP345_GAMEOBSERVERS_H
+#include <Vector>
+class Player;
+using namespace std;
 
-#endif //TEAMPROJECT_COMP345_GAMEOBSERVERS_H
+class Observer{
+public:
+    Observer();
+    ~Observer();
+    virtual void Update() = 0;
+
+//protected:
+//    Observer():
+
+ } ;
+
+class Observable{
+public:
+    virtual ~Observable(){};
+    virtual void attach(Observer *) = 0;
+    virtual void detach(Observer *) = 0;
+    virtual void notify() = 0;
+protected:
+    Observable(){};
+private:
+    vector<Observer*> observers_;
+};
+
+class Subject: public Observable{
+private:
+    vector<Observer*> *observers_;
+public:
+    virtual void Attach(Observer* o);
+    virtual void Detach(Observer* o);
+    virtual void Notify();
+    Subject();
+    ~Subject();
+};
+
+class GameStatisticObserver: Observer{
+private:
+    vector<Player*>* Players_;
+
+public:
+    GameStatisticObserver();
+    GameStatisticObserver(Player players);
+    ~GameStatisticObserver();
+
+    void update();
+};
+
+
+
