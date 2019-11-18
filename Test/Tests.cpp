6afcdeaa2f55571ed3db6e11b43011fed0997414
Giselle Martel <gmartel90@gmail.com>
2019-11-17 19:04:05 -0500

    finalized test cases for part 1 and part 2

--- Test/Tests.cpp
+++ Test/Tests.cpp
@@ -15,54 +15,54 @@ public:
     static void TestComputerPlayers() {
 
         GameEngine* new_game = new GameEngine();
-        PhaseObserver* observer = new PhaseObserver();
-        new_game->Register(observer);
+        //PhaseObserver* observer = new PhaseObserver();
+        //new_game->Register(observer);
 
         //test with 1 human players and an aggressive player
         new_game->TestAutoLoadMapAndCreateGame("../MapLoader/domination-map-files/generaltest.map", 1, 1 ,0);
 
-        //repeat game 10 times
-        for(int i = 0; i < 10; ++i) {
-            for(Player* player : *new_game->GetPlayers()) {
-                player->Reinforce();
-                player->Attack();
-                player->Fortify();
+        Player* human = nullptr;
+        Player* bot = nullptr;
+
+        for(Player* player : *new_game->GetPlayers()) {
+            if(player->IsHuman()) {
+                human = player;
+            } else {
+                bot = player;
             }
         }
 
-//    // test with 2 aggressive and 1 benevolant player
+        if(!human || !bot) {
+            return;
+        }
 
+        human->Reinforce();
+        human->Attack();
+        human->Fortify();
 
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 0, 2,1);
-//    new_game->StartGameLoop();
+        cout << "Dynamically changing behaviour of " << *human->GetPlayerName() << endl;
+        ConcreteStrategies* strategies1 = new BenevolantComputerPlayerStrategy;
+        human->SetPlayerStrategy(strategies1);
 
-//    //test with 2 human players
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 2, 0 ,0);
-//    new_game->StartGameLoop();
+        human->Reinforce();
+        human->Attack();
+        human->Fortify();
 
+        bot->Reinforce();
+        bot->Attack();
+        bot->Fortify();
 
+        cout << "Dynamically changing behaviour of " << *bot->GetPlayerName() << endl;
+        ConcreteStrategies* strategies2 = new HumanPlayerStrategy;
+        bot->SetPlayerStrategy(strategies2);
 
-//
-//    // test with one of each
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 1, 1,1);
-//    new_game->StartGameLoop();
-//
-//    //test with 2 aggressive players
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 0, 2,0);
-//    new_game->StartGameLoop();
-//
-//    // test with 2 benevolant players
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 0, 0,2);
-//    new_game->StartGameLoop();
-//
-//    // test that behaviour of a give player can be changed dynamically at runtime
-//    new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 1, 1,1);
-//    new_game->StartGameLoop();
+        bot->Reinforce();
+        bot->Attack();
+        bot->Fortify();
 
-        new_game->Unregister(observer);
+       // new_game->Unregister(observer);
         new_game = nullptr;
         delete new_game;
-
     }
 
     static void TestPhaseObserver() {
@@ -72,7 +72,7 @@ public:
         new_game->Register(observer);
 
         //test with 1 human players and an aggressive player
-        new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 1, 1 ,0);
+        new_game->TestAutoLoadMapAndCreateGame("../MapLoader/test-map-files/generaltest.map", 1, 0 ,1);
 
         //repeat game 10 times
         for(int i = 0; i < 10; ++i) {
@@ -170,6 +170,7 @@ int main() {
                 Tester::TestComputerPlayers();
                 break;
             case 2:
+                Tester::TestPhaseObserver();
                 break;
             case 3:
                 break;
